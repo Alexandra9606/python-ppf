cd4f76fa16c4894442abc72911934a1b99080ba8
Cosmin Craciun <cosmin91ro@gmail.com>
2020-12-23 08:46:30 +0200

    Arrows instead of letters

--- ppf-ex10/snake/snake.py
+++ ppf-ex10/snake/snake.py
@@ -3,10 +3,10 @@ import os
 import keyboard
 from symbols import *
 
-H = 40
-W = 80
+H = 10
+W = 20
 SLEEP = 0.5
-MC = "#"
+MC = "#" 
 
 x = H//2
 y = 1
@@ -18,18 +18,18 @@ clearScreen = lambda: os.system('cls')
 
 def getDirectionAndChar(direction, c):
     # Read user key
-    if keyboard.is_pressed('w'):  
+    if keyboard.is_pressed('up'):  
         direction = "UP"
         c = UPC
-    elif keyboard.is_pressed('a'): 
+    elif keyboard.is_pressed('left'): 
         direction = "LEFT"
         c = LEFTC
 
-    elif keyboard.is_pressed('s'): 
+    elif keyboard.is_pressed('down'): 
         direction = "DOWN"
         c = DOWNC
 
-    elif keyboard.is_pressed('d'):  
+    elif keyboard.is_pressed('right'):  
         direction = "RIGHT"
         c = RIGHTC
 
@@ -54,7 +54,7 @@ def renderScreen(x, y, c):
     clearScreen()
     for i in range(H):
         for j in range(W):
-            if i==0 or i==H-1 or j == 0 or j==W-1:
+            if i==0 or i==H-1 or j == 0 or j==W-1: 
                 print(MC, end="")
             elif i == x and j == y:
                 print(c, end="")
